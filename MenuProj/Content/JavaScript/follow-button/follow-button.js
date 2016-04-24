c334e4aa94882169c3230d1bfbd384f7f5ecf4e0
joseph e <joseph e>
2016-04-24 15:30:20 +0300

    Follow button added

---
+++ MenuProj/Content/JavaScript/follow-button/follow-button.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+    debugger;
+    var element = $('.follow-scroll');
+    var originalY = element.offset().top;
+
+    // Space between element and top of screen (when scrolling)
+    var topMargin = 50;
+
+    // Should probably be set in CSS; but here just for emphasis
+    element.css('position', 'relative');
+
+    $(window).on('scroll', function (event) {
+        var scrollTop = $(window).scrollTop();
+
+        element.stop(false, false).animate({
+            top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+        }, 300);
+    });
+})(jQuery);
